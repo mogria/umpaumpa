880d53534783ef145e1ea0c6025d15e8fa56e6aa
Mogria <m0gr14@gmail.com>
2020-01-10 20:20:45 +0100

    add more pages and make the tests pass

--- punkevents/tests.py
+++ punkevents/tests.py
@@ -1,3 +1,4 @@
+from django.utils.translation import ugettext as _
 from django.test import Client
 from django.test import TestCase
 from uuid import uuid4
@@ -26,10 +27,19 @@ class PagesTestCase(TestCase):
         Event.objects.all().delete()
         response = self.c.get('/')
         self.assertTemplateUsed(response, 'punkevents/index.html')
-        self.assertContains(response, 'Nächsti Konzis')
+        self.assertContains(response, _('Upcoming Events'))
         self.assertNotContains(response, self.event.name)
         self.assertContains(response, 'Momentan gids kei nächsti Konzis.')
         assert response.status_code == 200
+
+    def testUpcomingEventsNoCalendarEvents(self):
+        Event.objects.all().delete()
+        response = self.c.get('/')
+        self.assertTemplateUsed(response, 'punkevents/index.html')
+        self.assertContains(response, _('Calendar'))
+        self.assertContains(response, _("Currently there are no event dates available in the calendar"))
+        assert response.status_code == 200
+
 
     def testSingleEvent(self):
         response = self.c.get('/event/' + str(self.event.id))
@@ -42,4 +52,17 @@ class PagesTestCase(TestCase):
     def testSingleEventNotFound(self):
         response = self.c.get('/event/' + str(uuid4()))
         assert response.status_code == 404
-        assert b'Page not found' in response.content
+        assert _('Page not found') in response.content.decode('utf-8')
+
+
+    def testByDate(self):
+        test_date = datetime.date(2019, 1, 5)
+        response = self.c.get('/date/' + test_date.isoformat())
+        test_date.isocalendar
+        self.assertContains(response, '2019')
+        self.assertContains(response, '5')
+
+    def testByDateNoEvents(self):
+        test_date = datetime.date(2019, 1, 1)
+        response = self.c.get('/date/' + test_date.isoformat())
+        self.assertContains(response, _('No event at this date'))
--- punkevents/views.py
+++ punkevents/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.utils.translation import gettext as _
 from .models import Event
+import datetime
 
 # Create your views here.
 def upcoming(request):
     return render(request, 'punkevents/index.html', {
-        'events': Event.objects.order_by('start')[:10]
+        'events': Event.objects.order_by('start')[:10],
+        'calendar_events': False, # Event.objects('start')
     })
 
 
@@ -19,16 +21,20 @@ def event(request, event_id):
         })
     except Event.DoesNotExist:
         raise Http404(_("Event with UUID %(uuid) was not found.") % {
-            'uuid': event_id,
+            'uuid': int(event_id),
         })
 
 def date(request, date):
+    parsed_date = datetime.datetime.strptime(date, "%Y-%m-%d" )
+
     try:
-        events = Event.objects.get(start=date)
+        events = Event.objects.get(start=parsed_date)
+        print(events)
     except Event.DoesNotExist:
         events = []
     return render(request, 'punkevents/date.html', {
-        'events': events
+        'events': events,
+        'date': parsed_date
     })
 
 
@@ -38,6 +44,4 @@ def venue(request, venue_id):
     })
 
 def page_not_found(request, *args, **kwargs):
-    response = render(request, 'punkevents/404.html', {})
-    response.status_code = 404
-    return response
+    return render(request, 'punkevents/404.html', status=404)
